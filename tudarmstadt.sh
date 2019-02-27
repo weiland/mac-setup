b7bc125daa8e557bb21df6a5f98445426b2e971f
Pascal Weiland <pasweiland@gmail.com>
2019-02-27 13:43:11 +0100

    Add more software for university

---
+++ tudarmstadt.sh
@@ -0,0 +1,12 @@
+#!\usr\bin\env sh
+
+# install software which is not on Homebrew
+
+# JFLAP
+# http://www.jflap.org/jflaptmp/
+curl -o ~/Downloads/JFLAP7.1.jar http://www.jflap.org/jflaptmp/july27-18/JFLAP7.1.jar
+
+# Espresso
+
+# SystemVerilog
+
