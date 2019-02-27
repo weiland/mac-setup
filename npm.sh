516ac4fae5e59ad495f8be402ba949d7bd245bc5
Pascal Weiland <pasweiland@gmail.com>
2019-02-27 13:39:06 +0100

    Add basic npm dependencies

---
+++ npm.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+if ! command -v node >/dev/null; then
+  echo "NodeJS is not installed"
+  exit 1
+fi
+
+if ! command -v npm >/dev/null; then
+  echo "There is no npm"
+  exit 1
+fi
+
+npmi() {
+  if ! command -v "$1" >/dev/null; then
+    npm i -g $1
+    echo "$1 installed"
+  else
+    echo "command $1 already exists"
+  fi
+}
+
+npmi 'hicat'
+npmi 'git-open'
+npmi 'git-recent'
+npmi 'diff-so-fancy'
+npmi 'serve'
+npmi 'tldr'
+npmi 'spoof'
+npmi 'npmignore'
+npmi 'fast-cli'
