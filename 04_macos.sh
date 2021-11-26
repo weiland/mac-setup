1a2c5767918e879ae636ba93c0bad1eb06006301
Pascal Weiland <weiland@users.noreply.github.com>
2021-11-26 11:15:21 +0100

    Disable font smoothing
    source: https://tonsky.me/blog/monitors/#turn-off-font-smoothing

--- 04_macos.sh
+++ 04_macos.sh
@@ -269,7 +269,8 @@ defaults write com.apple.screencapture show-thumbnail -bool false
 
 # Enable subpixel font rendering on non-Apple LCDs
 # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+# defaults write NSGlobalDomain AppleFontSmoothing -int 2
+defaults write -g AppleFontSmoothing -int 0
 
 # Enable HiDPI display modes (requires restart)
 sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
